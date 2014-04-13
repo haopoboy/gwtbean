791074f6c14c74af377eb70fc981bd0f0bfa1288
HaoPo <haopoboy@gmail.com>
2014-04-13 16:07:31 +0800

    Remove block.

--- src/main/java/org/gwtbean/client/GwtBean.java
+++ src/main/java/org/gwtbean/client/GwtBean.java
@@ -6,6 +6,5 @@ import com.google.gwt.core.client.EntryPoint;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class GwtBean implements EntryPoint {
-  public void onModuleLoad() {
-  }
+  public void onModuleLoad() {}
 }

e9834ce4226e4b8048e2907fd9b1772c44f45566
HaoPo <haopoboy@gmail.com>
2014-08-24 17:29:26 +0800

    New test method.

--- src/test/java/org/gwtbean/client/cellview/AbstractColumUnitTest.java
+++ src/test/java/org/gwtbean/client/cellview/AbstractColumUnitTest.java
@@ -1,11 +1,20 @@
 package org.gwtbean.client.cellview;
 
 import org.gwtbean.client.BeanObject;
+import org.gwtbean.client.MethodNotSupportedException;
 import org.junit.Test;
 
 import com.google.gwt.user.cellview.client.Column;
 
 public class AbstractColumUnitTest {
+	
+	private final AbstractColumn<BeanObject> impl = new AbstractColumn<BeanObject>("") {
+		
+		@Override
+		protected Column<BeanObject, ?> createColumn() {
+			return null;
+		}
+	};
 
 	@Test(expected = NullPointerException.class)
 	public void whenPropertyPathIsNull() {
@@ -16,5 +25,9 @@ public class AbstractColumUnitTest {
 				return null;
 			}};
 	}
-
+	
+	@Test(expected = MethodNotSupportedException.class)
+	public void asWidget() {
+		impl.asWidget();
+	}
 }
